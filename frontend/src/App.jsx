bd4866e934822879445477b5979a0dd308dc2cf5
mirulh <amirulhaqem122@gmail.com.com>
2024-03-31 20:51:17 +0800

    6) sixth phase completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Badge from 'react-bootstrap/Badge';
 import CartScreen from './screen/CartScreen';
 import SigninScreen from './screen/SigninScreen';
 import SignupScreen from './screen/SignupScreen';
+import ProfileScreen from './screen/ProfileScreen';
 
 function App() {
   const { state, dispatch: contextDispatch } = useContext(Store);
@@ -84,6 +85,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/" element={<HomeScreen />} />
---
+++ frontend/src/screen/ProfileScreen.jsx
@@ -0,0 +1,118 @@
+import React, { useContext, useReducer, useState } from 'react';
+import { Store } from '../Store';
+import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { getError } from '../utils';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false };
+    default:
+      return state;
+  }
+};
+
+export default function ProfileScreen() {
+  //1) get the user info from store
+  const { state, dispatch: contextDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const [name, setName] = useState(userInfo.name);
+  const [email, setEmail] = useState(userInfo.email);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+    loadingUpdate: false,
+  });
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      // if password is not matching, prevents it from continuing
+      toast.error('Password do not match');
+      return;
+    }
+    try {
+      const { data } = await axios.put(
+        '/api/users/profile',
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      console.log('Data', JSON.stringify(data, null, 2));
+      dispatch({
+        type: 'UPDATE_SUCCESS',
+      });
+      contextDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      toast.success('User updated successfully');
+    } catch (err) {
+      dispatch({
+        type: 'FETCH_FAIL',
+      });
+      toast.error(getError(err));
+    }
+  };
+  return (
+    <div className="container small-container">
+      <Helmet>
+        <title>User Profile</title>
+      </Helmet>
+      <h1 className="my-3">User Profile</h1>
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <div className="mb-3">
+          <Button type="submit">Update</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
